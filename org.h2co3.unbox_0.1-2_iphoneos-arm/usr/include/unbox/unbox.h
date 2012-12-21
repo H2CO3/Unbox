00885c0f4da889f7daf2d5c53fd625c33af10a69
H2CO3 <arpad.goretity@gmail.com>
2012-12-21 22:41:59 +0100

    and change this as well

---
+++ org.h2co3.unbox_0.1-2_iphoneos-arm/usr/include/unbox/unbox.h
@@ -0,0 +1,11 @@
+//
+// unbox.h
+// Unbox
+//
+// Created by Árpád Goretity on 09/11/2011
+// Licensed under a CreativeCommons Attribution 3.0 Unported License
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreFoundation/CoreFoundation.h>
+#import "UBClient.h"
